ece34f8bcfc55d31367b99dd9f31d0e90918c334
Saurav Adhikari <sauravads123@gmail.com>
2020-07-31 11:25:22 +0545

    Setup ingress controller for auth service and fixed routing problem in ingress 'www.' in host name

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/api/users/currentUser",(req,res)=>{
+    res.send("Current User!")
+})
+
+
+app.post("/api/users/signup",(req,res)=>{
+    res.send("Sign up user!")
+})
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`Auth service listening at port :: ${PORT} !!`)

d6be1400ed31fe0bf9a59dd02d5eefe0b4c7e2b8
Saurav Adhikari <sauravads123@gmail.com>
2020-08-07 11:57:59 +0545

    improv : setup tests for /api/users/signin

--- auth/src/routes/__tests__/signin.test.ts
+++ auth/src/routes/__tests__/signin.test.ts
@@ -1,3 +1,50 @@
 import request from "supertest";
 import { app } from "../../app";
 //Jest automatically awaits return statements
+it("fails when a email does not exist is supplied", async () => {
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@demo.com",
+      password: "12345678",
+    })
+    .expect(400);
+});
+
+//Jest automatically awaits return statements
+it("fails when a invalid password is supplied does not exist is supplied", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "12345678",
+    })
+    .expect(400);
+});
+
+it("responds with a cookie for valid credentials", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
+  expect(response.get("Set-Cookie")).toBeDefined();
+});

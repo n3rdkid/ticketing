3d76238a3f06882e891a2fc14c5d88ac5019dd2c
Saurav Adhikari <sauravads123@gmail.com>
2020-08-11 09:15:10 +0545

    fix: cross namespace communication problem

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,30 @@
-export default () => {
-    return <h1 > Hello 2 123 </h1>
-}
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log("currentUser", currentUser);
+  return <h1> Landing Page </h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    //We are on the server
+    //request should be made to http://ingress-nginx.ingress-nginx.
+    //http://Service_Name.NAMESPACE.svc.cluster.local
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser"
+    ,{
+        headers:{
+            //Specifying domain in ingress-nginx
+            Host:"ticketing.dev"
+        }
+    });
+    return data;
+  } else {
+    //we are on the client
+    //request should be made with base url ""
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+  return {};
+};
+export default LandingPage;
